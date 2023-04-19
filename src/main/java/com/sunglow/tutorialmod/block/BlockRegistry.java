34c14448fc14d6d0bc961e455e4ff6e0c2ff1309
Sunglow1165 <xueyuntong90@gmail.com>
2023-04-19 18:03:25 +0800

    添加方块

--- src/main/java/com/sunglow/tutorialmod/block/BlockRegistry.java
+++ src/main/java/com/sunglow/tutorialmod/block/BlockRegistry.java
@@ -1,11 +1,16 @@
 package com.sunglow.tutorialmod.block;
 
 import com.sunglow.tutorialmod.TutorialMod;
+import com.sunglow.tutorialmod.common.ModTab;
 import com.sunglow.tutorialmod.item.ItemRegistry;
+import net.minecraft.util.valueproviders.UniformInt;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.DropExperienceBlock;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.Material;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
@@ -23,15 +28,28 @@ public class BlockRegistry {
     //方块延迟寄存器
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, TutorialMod.MOD_ID);
 
+    //锆石块
+    public static final RegistryObject<Block> ZIRCON_BLOCK = registerBlock("zircon_block",
+            () -> new Block(BlockBehaviour.Properties.of(Material.STONE).strength(6F).requiresCorrectToolForDrops()),
+            ModTab.MODE_TAB);
+    //锆石矿
+    public static final RegistryObject<Block> ZIRCON_ORE = registerBlock("zircon_ore",
+            () -> new DropExperienceBlock(BlockBehaviour.Properties.of(Material.STONE).strength(6F).requiresCorrectToolForDrops(),
+                    UniformInt.of(3, 7)), ModTab.MODE_TAB);
+    //深板岩锆石矿
+    public static final RegistryObject<Block> DEEPSLATE_ZIRCON_ORE = registerBlock("deepslate_zircon_ore",
+            () -> new DropExperienceBlock(BlockBehaviour.Properties.of(Material.STONE).strength(6F).requiresCorrectToolForDrops(),
+                    UniformInt.of(3, 7)), ModTab.MODE_TAB);
 
+    //注册方块
     private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block, CreativeModeTab tab) {
         RegistryObject<T> toReturn = BLOCKS.register(name, block);
         registerBlockItem(name, toReturn, tab);
         return toReturn;
     }
 
-    private static <T extends Block> RegistryObject<Item> registerBlockItem(String name, RegistryObject<T> block,
-                                                                            CreativeModeTab tab) {
+    //注册方块对应的物品
+    private static <T extends Block> RegistryObject<Item> registerBlockItem(String name, RegistryObject<T> block, CreativeModeTab tab) {
         return ItemRegistry.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties().tab(tab)));
     }
 }
